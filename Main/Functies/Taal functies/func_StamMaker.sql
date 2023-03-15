c1740d59f1c55da880a938c47b55828ee4006105
Rens Kersbergen <renskersbergen@hotmail.nl>
2023-03-15 18:03:52 +0100

    Func_stammaker

---
+++ Main/Functies/Taal functies/func_StamMaker.sql
@@ -0,0 +1,22 @@
+USE [VOICE_CONTROLLED_PARKINGFACILITY];
+GO
+
+CREATE OR ALTER FUNCTION func_StamMaker
+(
+	@Werkwoord VARCHAR(30)
+)
+
+RETURNS VARCHAR(30)
+AS
+BEGIN
+
+	DECLARE @StamResult VARCHAR
+	IF
+		NOT EXISTS(SELECT '' FROM ZwakkeWW zww WHERE zww.Stam = @Werkwoord)
+		AND NOT EXISTS(SELECT '' FROM SterkeWW sww WHERE sww.Stam = @Werkwoord)
+	BEGIN
+		SELECT @StamResult = (LEFT(@Werkwoord, LEN(@Werkwoord) - 1))
+	END
+
+	RETURN @StamResult
+END
